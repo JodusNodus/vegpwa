d802b2dc77451fde1d95fc85f17e299694f3e03f
jodusnodus <billiet.thomas@outlook.com>
2018-05-01 19:12:52 +0200

    Update styling

--- src/ui/views/LoginView.js
+++ src/ui/views/LoginView.js
@@ -6,7 +6,6 @@ import styles from "./LoginView.styles";
 import { withStyles } from "material-ui/styles";
 import { InputAdornment } from "material-ui/Input";
 import TextField from "material-ui/TextField";
-import Grid from "material-ui/Grid";
 import Paper from "material-ui/Paper";
 import Typography from "material-ui/Typography";
 import Button from "material-ui/Button";
@@ -74,80 +73,78 @@ class LoginView extends React.Component {
     const { classes } = this.props;
     const { fields, errors, showPassword } = this.state;
     return (
-      <Grid container className={classes.root}>
-        <Grid item xs={12} sm={8} md={6}>
-          <Paper elevation={4} className={classes.paper}>
-            <Typography variant="headline" component="h3">
-              Account aanmaken
-            </Typography>
+      <div className={classes.root}>
+        <Paper elevation={4} className={classes.paper}>
+          <Typography variant="headline" component="h3">
+            Account aanmaken
+          </Typography>
 
-            <TextField
-              label="Voornaam"
-              className={classes.input}
-              onChange={this.handleFieldChange("firstname")}
-              error={!!errors.firstname}
-              value={fields.firstname}
-              fullWidth
-            />
+          <TextField
+            label="Voornaam"
+            className={classes.input}
+            onChange={this.handleFieldChange("firstname")}
+            error={!!errors.firstname}
+            value={fields.firstname}
+            fullWidth
+          />
 
-            <TextField
-              label="Achternaam"
-              className={classes.input}
-              onChange={this.handleFieldChange("lastname")}
-              error={!!errors.lastname}
-              value={fields.lastname}
-              fullWidth
-            />
+          <TextField
+            label="Achternaam"
+            className={classes.input}
+            onChange={this.handleFieldChange("lastname")}
+            error={!!errors.lastname}
+            value={fields.lastname}
+            fullWidth
+          />
 
-            <TextField
-              fullWidth
-              label="Email"
-              type="email"
-              className={classes.input}
-              onChange={this.handleFieldChange("email")}
-              error={!!errors.email}
-              value={fields.email}
-            />
+          <TextField
+            fullWidth
+            label="Email"
+            type="email"
+            className={classes.input}
+            onChange={this.handleFieldChange("email")}
+            error={!!errors.email}
+            value={fields.email}
+          />
 
-            <TextField
-              fullWidth
-              label="Paswoord"
-              className={classes.input}
-              onChange={this.handleFieldChange("password")}
-              error={!!errors.password}
-              type={showPassword ? "text" : "password"}
-              value={fields.password}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="Toggle password visibility"
-                      onClick={this.handleClickShowPassword}
-                    >
-                      {this.state.showPassword ? (
-                        <VisibilityOffIcon />
-                      ) : (
-                        <VisibilityIcon />
-                      )}
-                    </IconButton>
-                  </InputAdornment>
-                )
-              }}
-            />
+          <TextField
+            fullWidth
+            label="Paswoord"
+            className={classes.input}
+            onChange={this.handleFieldChange("password")}
+            error={!!errors.password}
+            type={showPassword ? "text" : "password"}
+            value={fields.password}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="Toggle password visibility"
+                    onClick={this.handleClickShowPassword}
+                  >
+                    {this.state.showPassword ? (
+                      <VisibilityOffIcon />
+                    ) : (
+                      <VisibilityIcon />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+          />
 
-            <div className={classes.btnContainer}>
-              <Button
-                variant="raised"
-                color="primary"
-                className={classes.btn}
-                onClick={this.handleSignupClick}
-              >
-                Signup
-              </Button>
-            </div>
-          </Paper>
-        </Grid>
-      </Grid>
+          <div className={classes.btnContainer}>
+            <Button
+              variant="raised"
+              color="primary"
+              className={classes.btn}
+              onClick={this.handleSignupClick}
+            >
+              Signup
+            </Button>
+          </div>
+        </Paper>
+      </div>
     );
   }
 }
--- src/ui/views/LoginView.styles.js
+++ src/ui/views/LoginView.styles.js
@@ -1,12 +1,14 @@
 export default theme => ({
   root: {
+    display: "flex",
     justifyContent: "center",
     alignItems: "center",
     flexGrow: 1
   },
   paper: {
     padding: 25,
-    margin: 10
+    margin: 10,
+    maxWidth: 330
   },
   input: {
     marginTop: 10
