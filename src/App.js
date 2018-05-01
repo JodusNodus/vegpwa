f0a78e4bea48526219fee432d5856ea792b450c8
jodusnodus <billiet.thomas@outlook.com>
2018-05-01 20:00:15 +0200

    Fix route loading

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
+import { observer, inject } from "mobx-react";
 
-import { inject, observer } from "mobx-react";
 import { spring, AnimatedSwitch } from "react-router-transition";
 import { Route } from "react-router-dom";
-
-import Snackbar from "material-ui/Snackbar";
 
 import SplashView from "./ui/views/SplashView";
 import LoginView from "./ui/views/LoginView";
 import HomeView from "./ui/views/HomeView";
 import ProductView from "./ui/views/ProductView";
 import CreateProductView from "./ui/views/CreateProductView";
+import Snackbar from "material-ui/Snackbar";
 
 function mapStyles(styles) {
   return {
@@ -46,39 +45,45 @@ const styles = {
 
 @inject("connectionStore")
 @observer
+class ConnectionSnackBar extends React.Component {
+  render() {
+    return (
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center"
+        }}
+        open={!this.props.connectionStore.onLine}
+        autoHideDuration={null}
+        SnackbarContentProps={{
+          "aria-describedby": "message-id"
+        }}
+        message={<span id="message-id">U bent offline</span>}
+      />
+    );
+  }
+}
+
 export default class App extends React.Component {
   render() {
     return (
-      <Route path="/">
-        <div style={styles.root}>
-          <AnimatedSwitch
-            atEnter={bounceTransition.atEnter}
-            atLeave={bounceTransition.atLeave}
-            atActive={bounceTransition.atActive}
-            mapStyles={mapStyles}
-            className="switch-wrapper"
-          >
-            <Route path="/signup" component={LoginView} />
-            <Route path="/home" component={HomeView} />
-            <Route path="/product/:ean" component={ProductView} />
-            <Route path="/create" component={CreateProductView} />
-            <Route path="/splash" component={SplashView} />
-          </AnimatedSwitch>
+      <div style={styles.root}>
+        <AnimatedSwitch
+          atEnter={bounceTransition.atEnter}
+          atLeave={bounceTransition.atLeave}
+          atActive={bounceTransition.atActive}
+          mapStyles={mapStyles}
+          className="switch-wrapper"
+        >
+          <Route path="/signup" component={LoginView} />
+          <Route path="/home" component={HomeView} />
+          <Route path="/product/:ean" component={ProductView} />
+          <Route path="/create" component={CreateProductView} />
+          <Route path="/splash" component={SplashView} />
+        </AnimatedSwitch>
 
-          <Snackbar
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "center"
-            }}
-            open={!this.props.connectionStore.onLine}
-            autoHideDuration={null}
-            SnackbarContentProps={{
-              "aria-describedby": "message-id"
-            }}
-            message={<span id="message-id">U bent offline</span>}
-          />
-        </div>
-      </Route>
+        <ConnectionSnackBar />
+      </div>
     );
   }
 }
