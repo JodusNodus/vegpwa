97babb0a625d6e4cde161705bc08bc0d6dad29cd
jodusnodus <billiet.thomas@outlook.com>
2018-05-02 11:26:39 +0200

    Update path in build

--- src/services/navigation.js
+++ src/services/navigation.js
@@ -1,6 +1,8 @@
 import createHistory from "history/createBrowserHistory";
 
-export const history = createHistory();
+export const history = createHistory({
+  basename: "/vegpwa"
+});
 
 export const toSplash = () => history.replace("/splash");
 export const toSignup = () => history.replace("/signup");
