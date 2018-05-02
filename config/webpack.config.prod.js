ec0fae057d9398c6b379d13f14d15d1611b417c7
jodusnodus <billiet.thomas@outlook.com>
2018-05-02 11:46:59 +0200

    Update build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -310,6 +310,7 @@ module.exports = {
         }
         console.log(message);
       },
+      staticFileGlobs: ["https://storage.googleapis.com/vegstorage/cover-*"],
       minify: true,
       // For unknown URLs, fallback to the index page
       navigateFallback: publicUrl + "/index.html",
