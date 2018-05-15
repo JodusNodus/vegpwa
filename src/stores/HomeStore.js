ffcba80f8bace1f7af8d61310b93ee9e70b75016
jodusnodus <billiet.thomas@outlook.com>
2018-05-15 17:20:59 +0200

    Load more products at a time

--- src/stores/HomeStore.js
+++ src/stores/HomeStore.js
@@ -9,19 +9,25 @@ export default class HomeStore {
 
   fetchNewProducts = flow(function*() {
     if (this.newProducts.length) return;
-    const { products } = yield api.fetchProducts({ orderby: "creationdate" });
+    const { products } = yield api.fetchProducts({
+      orderby: "creationdate",
+      size: 20
+    });
     this.newProducts.replace(products);
   });
 
   fetchHighestRatedProducts = flow(function*() {
     if (this.highestRatedProducts.length) return;
-    const { products } = yield api.fetchProducts({ orderby: "rating" });
+    const { products } = yield api.fetchProducts({
+      orderby: "rating",
+      size: 20
+    });
     this.highestRatedProducts.replace(products);
   });
 
   fetchPopularProducts = flow(function*() {
     if (this.popularProducts.length) return;
-    const { products } = yield api.fetchProducts({ orderby: "hits" });
+    const { products } = yield api.fetchProducts({ orderby: "hits", size: 20 });
     this.popularProducts.replace(products);
   });
 }
